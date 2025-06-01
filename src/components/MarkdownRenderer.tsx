eca801d445811ac774d40d61970d8b5d73485fb4
Alex Gleason <alex@alexgleason.me>
2025-06-01 17:04:28 -0500

    Use dark theme syntax highlighting

--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -1,7 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeHighlight from 'rehype-highlight';
-import 'highlight.js/styles/github.css';
+import 'highlight.js/styles/github-dark.css';
 import { CodeBlock } from '@/components/CodeBlock';
 import { HeaderWithAnchor } from '@/components/HeaderWithAnchor';
 
